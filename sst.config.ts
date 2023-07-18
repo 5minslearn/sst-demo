85b9776708abba3447da555392c9f329f5f9678d
aruna-vr <aruna@gogosoon.com>
2023-07-18 12:12:44 +0530

    [Aruna] adds S3 upload feature

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,5 @@
 import { SSTConfig } from "sst";
-import { NextjsSite } from "sst/constructs";
+import {Bucket, NextjsSite } from "sst/constructs";
 
 export default {
   config(_input) {
@@ -10,8 +10,10 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site");
-
+      const bucket = new Bucket(stack, "public");
+      const site = new NextjsSite(stack, "site",{
+        bind:[bucket],
+      });
       stack.addOutputs({
         SiteUrl: site.url,
       });
